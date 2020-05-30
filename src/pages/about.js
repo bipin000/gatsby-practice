8336c6779d310a08ac6580125608fdacb54805b9
Bipin Chandra Tripathi <bipinchandratripathi@gmail.com>
2020-05-30 13:40:26 +0530

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <Header headerText="About Gatsby ll" />
+        <p>Such wow. Very React.</p>
+      </div>
+    </Layout>
+  )
+}
